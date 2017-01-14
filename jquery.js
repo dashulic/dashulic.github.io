00cc79ffb2aeb94cc48cbbff1939df26ac2ccfc7
dashulic <dpushkar@edu.uwaterloo.ca>
2017-01-14 18:20:04 -0500

    Add files via upload

--- jquery.js
+++ jquery.js
@@ -11,6 +11,17 @@
  *
  * Date: 2014-12-17T15:27Z
  */
+ 
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  3000);
 
 (function( global, factory ) {
 
